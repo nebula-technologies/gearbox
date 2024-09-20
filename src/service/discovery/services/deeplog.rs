3f36e6f4b4fa5fa2e55a5ea4df3c4aac297999ac
KatsuoRyuu <silwerspawn@gmail.com>
2024-09-20 04:07:05 +0000

    initial implementation for error downcast

---
+++ src/service/discovery/services/deeplog.rs
@@ -0,0 +1,233 @@
+use crate::common::get_ips;
+use crate::log::tracing::formatter::deeplog::DeepLogFormatter;
+use crate::service::discovery::entity::discovery::DiscoveryMessage;
+use crate::service::discovery::entity::{Broadcast, Config};
+use crate::service::discovery::DiscoveryService;
+use crate::time::DateTime;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use std::time::Duration;
+use tokio::net::UdpSocket;
+use tokio::task;
+use tokio::task::JoinHandle;
+use tokio::time::interval;
+
+impl DiscoveryService for DeepLogFormatter {
+    fn set_service_config<O: Fn(Config) -> Config>(self, o: O) -> Self {
+        let mut t = self.discovery_config.write();
+        *t = Some(o(t.clone().unwrap_or(Config::default())));
+
+        self
+    }
+
+    fn start_broadcast(self) -> (Self, JoinHandle<()>) {
+        let broadcast_config = self.discovery_config.clone();
+        (
+            self,
+            task::spawn(async move {
+                loop {
+                    let (ip, port, bcast_port, bcast_interval) = {
+                        let bcast = if let Some(c) =
+                            broadcast_config.read().as_ref().map(|t| &t.broadcast)
+                        {
+                            c.clone()
+                        } else {
+                            Broadcast::default()
+                        };
+                        (
+                            bcast
+                                .clone()
+                                .ip
+                                .unwrap_or(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0))),
+                            bcast.clone().port.unwrap_or(0),
+                            bcast.clone().bcast_port.unwrap_or(9999),
+                            bcast.clone().bcast_interval.unwrap_or(5),
+                        )
+                    };
+
+                    // Bind a socket to send broadcast messages
+                    let socket = match UdpSocket::bind(SocketAddr::new(ip, port)).await {
+                        Ok(socket) => {
+                            if let Err(e) = socket.set_broadcast(true) {
+                                println!(
+                                    "Failed to switch broadcast socket into broadcast mode: {}",
+                                    e
+                                );
+                                continue;
+                            }
+                            socket
+                        }
+                        Err(e) => {
+                            println!("Failed to bind the broadcast socket: {}", e);
+                            continue;
+                        }
+                    };
+
+                    loop {
+                        let (ip, port, service_name, version) = {
+                            let message = if let Some(c) =
+                                broadcast_config.read().as_ref().map(|t| &t.message)
+                            {
+                                c.clone()
+                            } else {
+                                DiscoveryMessage::default()
+                            };
+                            (
+                                message.ip.unwrap_or(get_ips()),
+                                message.port.unwrap_or(5000),
+                                message.service_name.unwrap_or("<Unknown>".to_string()),
+                                message.version.unwrap_or("v1.0.0".to_string()),
+                            )
+                        };
+
+                        let discovery_message = DiscoveryMessage {
+                            additional_info: None,
+                            ip: Some(ip),
+                            mac: None,
+                            port: Some(port),
+                            service_name: Some(service_name),
+                            timestamp: Some(DateTime::now()),
+                            version: Some(version),
+                            http: false,
+                            http_api_schema_endpoint: None,
+                        };
+
+                        let discovery_payload = if let Ok(t) =
+                            serde_json::to_string(&discovery_message)
+                        {
+                            t
+                        } else {
+                            println!("Failed to convert payload into a sendable buffer/string");
+                            tokio::time::sleep(Duration::from_secs(bcast_interval as u64)).await;
+                            continue;
+                        };
+
+                        socket
+                            .send_to(
+                                discovery_payload.as_bytes(),
+                                format!("255.255.255.255:{}", bcast_port),
+                            )
+                            .await
+                            .ok();
+                        println!(
+                            "Broadcasting discovery message: {:<40}",
+                            discovery_message.to_string()
+                        );
+
+                        tokio::time::sleep(Duration::from_secs(bcast_interval as u64)).await;
+                    }
+                }
+            }),
+        )
+    }
+
+    fn start_discovery(self) -> (Self, JoinHandle<()>) {
+        // Clone the logging config Arc<RwLock>
+        let config = if let Some(_) = self.discovery_config.detach().clone() {
+            self.discovery_config.clone()
+        } else {
+            *self.discovery_config.write() = Some(Config::default());
+            self.discovery_config.clone()
+        };
+
+        // Spawn a background task to listen for discovery messages and update the config
+        (
+            self,
+            task::spawn(async move {
+                // Retrieve the discovery config from the cloned logging config
+                loop {
+                    let (ip, port, bcast_port, bcast_interval) = {
+                        let bcast = if let Some(c) = config.read().as_ref().map(|t| &t.broadcast) {
+                            c.clone()
+                        } else {
+                            Broadcast::default()
+                        };
+                        (
+                            bcast
+                                .clone()
+                                .ip
+                                .unwrap_or(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0))),
+                            bcast.clone().port.unwrap_or(0),
+                            bcast.clone().bcast_port.unwrap_or(9999),
+                            bcast.clone().bcast_interval.unwrap_or(5),
+                        )
+                    };
+
+                    let mut interval = interval(Duration::from_secs(bcast_interval as u64));
+
+                    // Attempt to bind the socket
+                    let socket = match UdpSocket::bind(SocketAddr::new(ip, bcast_port)).await {
+                        Ok(sock) => {
+                            println!("Listening for discovery messages on {}:{}", ip, bcast_port);
+                            sock
+                        }
+                        Err(e) => {
+                            println!("Failed to bind socket on {}: {}. Retrying...", ip, e);
+                            interval.tick().await;
+                            continue; // Retry the loop on failure
+                        }
+                    };
+
+                    let mut buf = [0u8; 1024]; // Buffer to store the incoming message
+
+                    loop {
+                        interval.tick().await;
+
+                        // Receive a message from the socket
+                        match socket.recv_from(&mut buf).await {
+                            Ok((len, src)) => {
+                                let received_msg = String::from_utf8_lossy(&buf[..len]);
+
+                                println!(
+                                    "Received discovery message from: {:<20} for {}",
+                                    src,
+                                    serde_json::from_str::<DiscoveryMessage>(&received_msg)
+                                        .map(|t| t.to_string())
+                                        .unwrap_or("<failed>".to_string())
+                                );
+
+                                let svc_name = config
+                                    .read()
+                                    .as_ref()
+                                    .and_then(|t| t.message.service_name.clone());
+
+                                let discovery_message = if let Ok(t) =
+                                    serde_json::from_str::<DiscoveryMessage>(received_msg.as_ref())
+                                {
+                                    t
+                                } else {
+                                    continue;
+                                };
+
+                                if let (Some(r), Some(s)) =
+                                    (discovery_message.service_name.as_ref(), svc_name)
+                                {
+                                    if r != &s {
+                                        continue;
+                                    }
+                                } else {
+                                    continue;
+                                }
+
+                                // Lock the config for write access to update the discovery info
+                                if let Some(config_rw) = config.write().as_mut() {
+                                    config_rw.message = discovery_message.clone();
+
+                                    println!("Updated config with discovery message from {}", src);
+                                } else {
+                                    println!("No discovery config found in logging config.");
+                                }
+                            }
+                            Err(e) => {
+                                println!(
+                                    "Error receiving discovery message: {}. Restarting socket...",
+                                    e
+                                );
+                                break; // Break to restart the socket
+                            }
+                        }
+                    }
+                }
+            }),
+        )
+    }
+}
---
+++ src/service/framework/axum/mod.rs
@@ -0,0 +1,312 @@
+use axum::serve::IncomingStream;
+use axum::{Router, ServiceExt};
+use gearbox::log::service::discovery::DiscoveryService;
+use gearbox::log::tracing::formatter::bunyan::Bunyan;
+use gearbox::log::tracing::formatter::deeplog::DeepLogFormatter;
+use gearbox::log::tracing::formatter::syslog::Syslog;
+use gearbox::log::tracing::layer::LogLayer;
+use std::any::{Any, TypeId};
+use std::collections::HashMap;
+use std::marker::PhantomData;
+use std::net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr};
+use std::sync::Arc;
+use std::time::Duration;
+use tokio::signal;
+use tokio::sync::RwLock;
+use tracing::event;
+use tracing::Level;
+use tracing_subscriber::{layer::SubscriberExt, Registry};
+
+#[derive(Clone)]
+pub struct AppStateBuilder {
+    // A map for storing application state keyed by TypeId
+    state: HashMap<TypeId, Arc<dyn Any + Send + Sync>>,
+}
+
+impl AppStateBuilder {
+    pub fn add<T: 'static + Default + Send + Sync>(&mut self) -> &mut Self {
+        let state = T::default();
+        self.state.insert(state.type_id(), Arc::new(T::default()));
+        self
+    }
+
+    fn build(self) -> Arc<AppState> {
+        Arc::new(AppState {
+            state: self.clone().state,
+        })
+    }
+}
+
+impl Default for AppStateBuilder {
+    fn default() -> Self {
+        AppStateBuilder {
+            state: HashMap::new(),
+        }
+    }
+}
+
+#[derive(Clone)]
+pub struct AppState {
+    // A map for storing application state keyed by TypeId
+    state: HashMap<TypeId, Arc<dyn Any + Send + Sync>>,
+}
+
+impl AppState {
+    // Create a new AppState
+    pub fn new(state: HashMap<TypeId, Arc<dyn Any + Send + Sync>>) -> Self {
+        Self { state }
+    }
+
+    // Get a reference to a value in the state by type
+    pub async fn get<T: Any + Send + Sync>(&self) -> Option<&T> {
+        self.state
+            .get(&TypeId::of::<T>())
+            .and_then(|boxed| boxed.downcast_ref::<T>())
+    }
+}
+
+pub struct ModuleBuilder<'a> {
+    router: &'a mut Router,
+}
+
+impl<'a> ModuleBuilder<'a> {
+    pub fn new(router: &'a mut Router) -> Self {
+        Self { router }
+    }
+    pub fn router<O: FnOnce(&mut Router)>(mut self, o: O) -> Self {
+        o(&mut self.router);
+        self
+    }
+}
+#[derive(Debug, Clone)]
+pub struct DiscoveryBuilder {
+    ip: Option<IpAddr>,
+    port: Option<u16>,
+    interval: Option<usize>,
+    service_name: Option<String>,
+}
+
+impl DiscoveryBuilder {
+    pub fn new() -> Self {
+        DiscoveryBuilder {
+            interval: Some(5),
+            ip: Some(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0))),
+            port: Some(9999),
+            service_name: Some("Log-service".to_string()),
+        }
+    }
+
+    pub fn set_ip(mut self, ip: IpAddr) -> Self {
+        self.ip = Some(ip);
+        self
+    }
+
+    pub fn set_port(mut self, port: u16) -> Self {
+        self.port = Some(port);
+        self
+    }
+
+    pub fn set_interval(mut self, interval: usize) -> Self {
+        self.interval = Some(interval);
+        self
+    }
+
+    pub fn set_service_name(mut self, service_name: &str) -> Self {
+        self.service_name = Some(service_name.to_string());
+        self
+    }
+}
+
+pub enum LogStyle {
+    Bunyan,
+    DeepLog,
+    Syslog,
+}
+
+pub struct ServerBuilder {
+    address: IpAddr,
+    port: u16,
+    worker_pool: Option<usize>,
+    router: Router,
+    logger: LogStyle,
+    logger_discovery: bool,
+    logger_discovery_builder: Option<DiscoveryBuilder>,
+    trace_layer: bool,
+    app_state: Option<AppStateBuilder>,
+}
+
+impl ServerBuilder {
+    fn new() -> Self {
+        Self {
+            address: IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)),
+            port: 3000,
+            worker_pool: None,
+            router: Router::new(),
+            logger: LogStyle::DeepLog,
+            logger_discovery: false,
+            logger_discovery_builder: None,
+            trace_layer: false,
+            app_state: None,
+        }
+    }
+
+    fn set_address(mut self, ip: &[u16]) -> Self {
+        if ip.len() == 4 {
+            self.address = IpAddr::V4(Ipv4Addr::new(
+                ip[0] as u8,
+                ip[1] as u8,
+                ip[2] as u8,
+                ip[3] as u8,
+            ));
+        } else if ip.len() != 16 {
+            self.address = IpAddr::V6(Ipv6Addr::new(
+                ip[0], ip[1], ip[2], ip[3], ip[4], ip[5], ip[6], ip[7],
+            ));
+        } else {
+            panic!("Invalid IP address used");
+        }
+
+        self
+    }
+
+    fn with_trace_layer(mut self) -> Self {
+        self.trace_layer = true;
+        self
+    }
+    fn with_log_service_discovery<O: FnOnce(DiscoveryBuilder) -> Option<DiscoveryBuilder>>(
+        mut self,
+        o: O,
+    ) -> Self {
+        self.trace_layer = true;
+        self.logger_discovery_builder = o(DiscoveryBuilder::new());
+        self
+    }
+
+    fn set_port(mut self, port: u16) -> Self {
+        self.port = port;
+        self
+    }
+
+    fn add_module<O: FnOnce(ModuleBuilder) -> ModuleBuilder>(mut self, o: O) -> Self {
+        o(ModuleBuilder::new(&mut self.router));
+
+        self
+    }
+
+    fn set_worker_pool(mut self, max_workers: usize) -> Self {
+        self.worker_pool = Some(max_workers);
+        self
+    }
+
+    fn build(self) {
+        println!("Building body closure");
+        let body = async {
+            println!("Creating app");
+            let mut app: Router = self.router;
+
+            if self.trace_layer {
+                app = app.layer((
+                    TraceLayer::new_for_http(),
+                    // Graceful shutdown will wait for outstanding requests to complete. Add a timeout so
+                    // requests don't hang forever.
+                    TimeoutLayer::new(Duration::from_secs(10)),
+                ));
+            }
+
+            setup_logger(
+                self.logger,
+                self.logger_discovery,
+                self.logger_discovery_builder,
+            );
+
+            println!("Setting up listener socket address");
+            let socket_addr = SocketAddr::new(self.address, self.port);
+            let listener = tokio::net::TcpListener::bind(socket_addr).await.unwrap();
+
+            async fn shutdown_signal() {
+                let ctrl_c = async {
+                    signal::ctrl_c()
+                        .await
+                        .expect("failed to install Ctrl+C handler");
+                };
+
+                #[cfg(unix)]
+                let terminate = async {
+                    signal::unix::signal(signal::unix::SignalKind::terminate())
+                        .expect("failed to install signal handler")
+                        .recv()
+                        .await;
+                };
+
+                #[cfg(not(unix))]
+                let terminate = std::future::pending::<()>();
+
+                tokio::select! {
+                    _ = ctrl_c => {},
+                    _ = terminate => {},
+                }
+            }
+
+            println!("Starting server");
+            axum::serve(listener, app)
+                .with_graceful_shutdown(shutdown_signal())
+                .await
+                .unwrap()
+        };
+
+        println!("Setting up the thread builder for tokio");
+        let mut builder = tokio::runtime::Builder::new_multi_thread();
+
+        if let Some(threads) = self.worker_pool.as_ref() {
+            println!("Using defined worker threads");
+            builder.worker_threads(*threads);
+        } else {
+            println!("Using auto-lookup worker threads");
+        }
+
+        builder
+            .enable_all()
+            .build()
+            .expect("Failed building the Runtime")
+            .block_on(body);
+    }
+}
+
+fn setup_logger(logger: LogStyle, discovery: bool, builder: Option<DiscoveryBuilder>) {
+    let mut formatter = match logger {
+        LogStyle::Bunyan => {
+            panic!("Bunyan not currently supported")
+        }
+        LogStyle::DeepLog => DeepLogFormatter::default(),
+        LogStyle::Syslog => {
+            panic!("Syslog not currently supported")
+        }
+    };
+
+    if discovery {
+        let (fmt, handle) = formatter
+            .set_service_config(|mut t| {
+                let mut current_discovery_builder = builder.clone();
+                let discovery_builder =
+                    current_discovery_builder.get_or_insert(DiscoveryBuilder::new());
+
+                t.broadcast.ip = discovery_builder.ip;
+                t.broadcast.bcast_port = discovery_builder.port;
+                t.message.service_name = discovery_builder.service_name.clone();
+                t.broadcast.bcast_interval = discovery_builder.interval.map(|t| t as u16);
+
+                t
+            })
+            .start_discovery();
+        formatter = fmt;
+    }
+
+    let formatter = LogLayer::new(None, std::io::stdout, formatter);
+    let subscriber = Registry::default().with(formatter);
+    tracing::subscriber::set_global_default(subscriber).expect("Failed to attach log subscriber");
+    event!(
+        Level::DEBUG,
+        level = "emergency",
+        "Testing subscriber with level override"
+    );
+}
