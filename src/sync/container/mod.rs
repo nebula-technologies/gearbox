eed49bbfaabad74f467f240e29b36ba6284e29c0
anders.pedersen@woven-planet.global <silwerspawn@gmail.com>
2025-01-27 11:30:02 +0900

    update

--- src/sync/container/mod.rs
+++ src/sync/container/mod.rs
@@ -1,6 +1,7 @@
 pub mod common_key_container;
 pub mod common_type_container;
 
+use crate::common::merge::DataMerge;
 use std::any::Any;
 use std::fmt::Debug;
 use std::sync::Arc;
@@ -12,6 +13,7 @@ pub trait TypeContainer: Clone + Default + Debug + Send + Sync {
         self.set(t);
         self
     }
+    fn update<V, T: DataMerge<V> + Any + Send + Sync>(&self, t: T) -> Option<Arc<T>>;
     fn get<T: Any + Send + Sync>(&self) -> Option<Arc<T>>;
     fn remove<T: Any + Send + Sync>(&self) -> Option<Arc<T>>;
     fn has<T: Any + Send + Sync>(&self) -> bool {
@@ -31,4 +33,11 @@ pub trait KeyContainer<K, V>: Clone + Default + Debug + Send + Sync {
     fn has(&self, key: &K) -> bool {
         self.get(key).is_some()
     }
+}
+
+pub trait KeyContainerExtMerge<K,V>: KeyContainer<K, V> {
+
+    fn update<VM: DataMerge<V>>(&self, key: K, value: VM) -> Option<Arc<V>>
+    where
+        V: DataMerge<V>;
 }
